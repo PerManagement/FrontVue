51ff25287ecddca5805c10aa541823f84603644b
xiehaihong <xiehaihong@163.com>
2020-04-29 21:23:53 +0800

    4.29

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex);
+//何辉 人事管理
 import login from './login'
+//谢海鸿 任务管理
 import task from './task'
+//龙珊 薪资管理
+//周怡珊 考勤管理
+//陈良吉 公告管理
 import getters from './getters'
 import axios from 'axios'
 import ElementUI from 'element-ui';
