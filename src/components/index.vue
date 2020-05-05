85005349807a1e87d82459320421c26a733b90fe
xiehaihong <xiehaihong@163.com>
2020-05-05 22:04:12 +0800

    合并longshan

--- src/components/index.vue
+++ src/components/index.vue
@@ -67,7 +67,7 @@
         </template>
           <el-menu-item index="/findWage" @click="findWage">查询记录</el-menu-item>
           <el-menu-item index="/issueWage" @click="issueWage">发放工资</el-menu-item>
-          <el-menu-item index="2-3">添加工资条</el-menu-item>
+          <!--<el-menu-item index="2-3">添加工资条</el-menu-item>-->
           <el-menu-item index="/findWageState" @click="findWageState">待审批薪资</el-menu-item>
           <el-menu-item index="/findWageByUserId" @click="findWageByUserId">工资条</el-menu-item>
       </el-submenu> 
