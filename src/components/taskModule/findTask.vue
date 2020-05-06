0c6eac019e9c431c5eb425d7125292f46218ab46
xiehaihong <xiehaihong@163.com>
2020-05-06 21:44:03 +0800

    5.6

--- src/components/taskModule/findTask.vue
+++ src/components/taskModule/findTask.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+    <div class="block" style="padding-bottom:10px">
+          <el-date-picker
+            value-format="yyyy-MM-dd"  
+            v-model="begindate"
+            align="right"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+          &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;
+          <el-date-picker
+            value-format="yyyy-MM-dd"  
+            v-model="enddate"
+            align="right"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>&nbsp;&nbsp;&nbsp;
+          <el-button type="primary" icon="el-icon-search" @click="find()">搜索</el-button>
+        </div>
+
         <el-table :data="pageInfo.list" border style="width: 100%" stripe @sort-change="changeSort"
         :default-sort = "{prop:'taskid',order:'descending'}" ref="multipleTable">
           <el-table-column v-for="item in props" :key="item.prop" :prop="item.prop" :label="item.label" 
@@ -38,6 +57,8 @@ export default {
       return {
         page:1,
         pageInfo:{},
+        begindate:'',
+        enddate:'',
         props:[
           {prop:"taskid",label:"编号",width:"100"},
           {prop:"taskname",label:"任务名称",width:"120"},
@@ -57,7 +78,7 @@ export default {
     methods:{
         //分页
         find(page=1,pageSize=6){
-          let url="task/findTask?page="+page+"&pageSize="+pageSize;
+          let url="task/findTask?page="+page+"&pageSize="+pageSize+"&begindate="+this.begindate+"&enddate="+this.enddate;
           this.$axios.get(url).then(resp=>{
             console.log(resp.data.data);
             this.pageInfo=resp.data.data;
